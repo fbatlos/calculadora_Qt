162b5ab2407fd32891e97ad4d83c21bcacdc6dec
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2025-02-07 20:24:40 +0100

    Cuncionalidad 100%, colores, demas bien, comentado y solo falta el .exe y el bash usado

--- calculadora.py
+++ calculadora.py
@@ -8,6 +8,8 @@ class Calculadora(QMainWindow):
     def __init__(self):
         super(Calculadora, self).__init__()
         uic.loadUi('calculadora.ui', self)
+
+        #Botones de numeros y operadores
         
         botones = {
             "Bt_0": "0", "Bt_1": "1", "Bt_2": "2", "Bt_3": "3", "Bt_4": "4", 
@@ -18,10 +20,13 @@ class Calculadora(QMainWindow):
         
         for boton, valor in botones.items():
             getattr(self, boton).clicked.connect(lambda _, v=valor: self.agregar_numero(v))
-        
+
+        #Botones de control
         self.Bt_clean.clicked.connect(self.borrar)
         self.Bt_cleanAll.clicked.connect(self.borrar_todo)
         self.Bt_igual.clicked.connect(self.evaluar_expresion)
+
+        #Botones de funciones y constantes
         
         self.Bt_seno.clicked.connect(lambda: self.agregar_funcion("sin"))
         self.Bt_coseno.clicked.connect(lambda: self.agregar_funcion("cos"))
@@ -34,6 +39,7 @@ class Calculadora(QMainWindow):
         self.Bt_factorial.clicked.connect(lambda: self.agregar_funcion("factorial"))
         self.Bt_expo.clicked.connect(lambda: self.agregar_numero("**"))
 
+        #Aspectos de la tabla
         self.TW_table.setColumnCount(2)
         self.TW_table.setHorizontalHeaderLabels(["Operación", "Resultado"])
         self.TW_table.setRowCount(0)
@@ -41,7 +47,17 @@ class Calculadora(QMainWindow):
         self.expresion = ""
         self.interno = ""
 
+        self.isFirst = True
+
+    #Funcion encargada de agregar un numero a la expresion, se muestra de forma diferente en el TE_resultado y en la variable interna
     def agregar_numero(self, numero):
+        if self.isFirst and numero.isdigit():
+            self.expresion = ""
+            self.interno = ""
+            self.isFirst = False
+        else:
+            self.isFirst = False
+
         if numero == "**":
             self.expresion += "^"
             self.interno += numero
@@ -54,6 +70,7 @@ class Calculadora(QMainWindow):
 
         self.TE_resultado.setText(self.expresion)
 
+    #Funcion encargada de agregar una constante a la expresion, se muestra de forma diferente en el TE_resultado y en la variable interna
     def agregar_constante(self, constante):
         showConstant = {
             "tau": "τ",
@@ -61,10 +78,16 @@ class Calculadora(QMainWindow):
             "pi": "π",
         }
 
-        self.expresion += f"{showConstant[constante]}"
-        self.interno += f"math.{constante}"
+        if self.interno[len(self.interno)-1].isdigit():
+            self.expresion += f"{showConstant[constante]}"
+            self.interno += f"*math.{constante}"
+        else:
+            self.expresion += f"{showConstant[constante]}"
+            self.interno += f"math.{constante}"
+
         self.TE_resultado.setText(self.expresion)
-    
+
+    #Funcion encargada de agregar una funcion a la expresion, se muestra de forma diferente en el TE_resultado y en la variable interna
     def agregar_funcion(self, funcion):
         showFunction = {
             "sin": "sin",
@@ -75,11 +98,16 @@ class Calculadora(QMainWindow):
             "factorial": "!"
         }
 
-        self.expresion += f"{showFunction[funcion]}("
-        self.interno += f"math.{funcion}("
+        if self.interno[len(self.interno)-1].isdigit():
+            self.expresion += f"{showFunction[funcion]}("
+            self.interno += f"*math.{funcion}("
+        else:
+            self.expresion += f"{showFunction[funcion]}("
+            self.interno += f"math.{funcion}("
+
         self.TE_resultado.setText(self.expresion)
 
-    
+    #Funcion encargada de borrar el ultimo caracter de la expresion, se comprueba si es una constante, funcion o numero para eliminar de forma diferente la variable interna
     def borrar(self):
         if self.expresion.endswith("τ"):
             self.expresion = self.expresion[:-1]
@@ -105,7 +133,8 @@ class Calculadora(QMainWindow):
         self.expresion = ""
         self.interno = ""
         self.TE_resultado.setText(self.expresion)
-    
+
+    #Aquí se evalua la expresión y se muestra el resultado en el TE_resultado y se guarda en el historial
     def evaluar_expresion(self):
         try:
             if self.interno == "":
@@ -116,14 +145,19 @@ class Calculadora(QMainWindow):
                 self.guardar_historial(self.expresion, str(resultado))
                 self.expresion = str(resultado)
                 self.interno = str(resultado)
+                self.isFirst = True
+
 
         except Exception as error:
-            print(type(error))
+            #Lugar donde se manejan los errores de sintaxis
+            print(self.interno)
             QMessageBox.critical(self,"Error", "Sintaxis incorrecta") 
             self.TE_resultado.setText("")
             self.expresion = ""
             self.interno = ""
-    
+
+
+    #Funcion encargada de guardar el historial de operaciones en surespectivo lugar
     def guardar_historial(self, operacion, resultado):
         row_position = self.TW_table.rowCount()
         self.TW_table.insertRow(row_position)
